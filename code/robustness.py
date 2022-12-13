de9076f940b279bf05985e70f4d994028983db49
abhinav-neil <abhinav.neil@gmail.com>
2022-12-13 07:06:43 +0100

    bug fixes & cleanup

--- code/robustness.py
+++ code/robustness.py
@@ -1,19 +1,3 @@
-################################################################################
-# MIT License
-#
-# Copyright (c) 2022 University of Amsterdam
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to conditions.
-#
-# Author: Deep Learning Course (UvA) | Fall 2022
-# Date Created: 2022-11-14
-################################################################################
-
 """Helper script to evaluate noise robustness."""
 import os
 import argparse
